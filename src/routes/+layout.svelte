cd3dbce9f3951632408d0ab06d44df707d268787
Henrik Schwebcke <58189782+overiit@users.noreply.github.com>
2024-06-16 08:27:33 +0200

    automatic year counter

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,31 +1,35 @@
 <script lang="ts">
-	import Header from "../components/header.svelte";
-	import Navigation from "../components/navigation.svelte";
-    
-    const DEFAULT_TITLE = "Hey! I'm Henrik Schwebcke";
-    let title = DEFAULT_TITLE;
-    let currentPage: string | undefined;
+	import Header from '../components/header.svelte';
+	import Navigation from '../components/navigation.svelte';
 
+	const DEFAULT_TITLE = "Hey! I'm Henrik Schwebcke";
+	let title = DEFAULT_TITLE;
+	let currentPage: string | undefined;
 </script>
-<svelte:window on:blur={() => title = "Hello?"} on:focus={() => title = DEFAULT_TITLE} />
+
+<svelte:window on:blur={() => (title = 'Hello?')} on:focus={() => (title = DEFAULT_TITLE)} />
 <svelte:head>
-    <title>{title}</title>
-    <meta name="description" content="A software engineer with 4+ years of experience delivering full-stack applications." />
+	<title>{title}</title>
+	<meta
+		name="description"
+		content="A software engineer with {new Date().getFullYear() -
+			2019}+ years of experience delivering full-stack applications."
+	/>
 </svelte:head>
 <main>
-    <Header />
-    <slot />
-    <Navigation onPageChange={page => currentPage = page} />
+	<Header />
+	<slot />
+	<Navigation onPageChange={(page) => (currentPage = page)} />
 </main>
 
 <style lang="scss">
-    main {
-        display: flex;
-        flex-flow: column;
-        background: linear-gradient(135deg, #50BCEB, #3BBE7F);
-        padding: 1.5rem;
-        height: calc(100% - 3rem);
-        max-height: 100vh;
-        justify-content: space-between;
-    }
+	main {
+		display: flex;
+		flex-flow: column;
+		background: linear-gradient(135deg, #50bceb, #3bbe7f);
+		padding: 1.5rem;
+		height: calc(100% - 3rem);
+		max-height: 100vh;
+		justify-content: space-between;
+	}
 </style>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,74 +1,74 @@
 <script>
-
-	import { scale } from "svelte/transition";
-
+	import { scale } from 'svelte/transition';
 </script>
-<section in:scale={{ delay: 0}}>
-    <svg-icon >
-        <svg viewBox="0 0 120 50">
-            <text y=25>HEY! I'M</text>
-        </svg>
-    </svg-icon>
-    <h1>HENRIK SCHWEBCKE</h1>
-    <p>
-        A <b>software engineer</b> with <b>4+ years</b> of experience<br />
-        delivering <b>full-stack</b> applications.
-    </p>
+
+<section in:scale={{ delay: 0 }}>
+	<svg-icon>
+		<svg viewBox="0 0 120 50">
+			<text y="25">HEY! I'M</text>
+		</svg>
+	</svg-icon>
+	<h1>HENRIK SCHWEBCKE</h1>
+	<p>
+		A <b>software engineer</b> with <b>{new Date().getFullYear() - 2019}+ years</b> of experience<br
+		/>
+		delivering <b>full-stack</b> applications.
+	</p>
 </section>
 
 <style lang="scss">
-    section {
-        overflow-y: auto;
-        overflow-x: hidden;
-        width: 1200px;
-        max-width: 100%;
-        margin: 0 auto;
-        color: white;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        flex-flow: column ;
-        svg-icon {
-            height: 35px;
-            width: 150px;
-        }
-        text-align: center;
-        svg{
-            font   : bold 30px 'Roboto', sans-serif;
-            text{
-                fill            : none;
-                stroke          : white;
-                stroke-width    : 1px;
-                stroke-linejoin : round;
-            }
-        }
-        h1 {
-            line-height: 3rem;
-            font-size: 3rem;
-            font-weight: 800;
-            padding: 0 .5rem;
-        }
-        p {
-            font-weight: 300;
-        }
-    }
+	section {
+		overflow-y: auto;
+		overflow-x: hidden;
+		width: 1200px;
+		max-width: 100%;
+		margin: 0 auto;
+		color: white;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-flow: column;
+		svg-icon {
+			height: 35px;
+			width: 150px;
+		}
+		text-align: center;
+		svg {
+			font: bold 30px 'Roboto', sans-serif;
+			text {
+				fill: none;
+				stroke: white;
+				stroke-width: 1px;
+				stroke-linejoin: round;
+			}
+		}
+		h1 {
+			line-height: 3rem;
+			font-size: 3rem;
+			font-weight: 800;
+			padding: 0 0.5rem;
+		}
+		p {
+			font-weight: 300;
+		}
+	}
 
-    @media only screen and (max-width: 600px) {
-        section {
-            width: 100vw;
-            max-width: 100vw;
-            margin-left: -1.5rem;
-            svg-icon {
-                height: 25px;
-                width: 100px;
-            }
-            h1 {
-                font-size: 2rem;
-                line-height: 2rem;
-            }
-            p {
-                font-size: 0.8rem;
-            }
-        }
-    }
+	@media only screen and (max-width: 600px) {
+		section {
+			width: 100vw;
+			max-width: 100vw;
+			margin-left: -1.5rem;
+			svg-icon {
+				height: 25px;
+				width: 100px;
+			}
+			h1 {
+				font-size: 2rem;
+				line-height: 2rem;
+			}
+			p {
+				font-size: 0.8rem;
+			}
+		}
+	}
 </style>
